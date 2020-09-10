b27a9a454b2aa9dd9e537231b98862ad5b2978e0
syedam1 <syed@am1.org>
2020-09-10 11:53:57 +0100

    updated with folder creation.

--- app/Http/Controllers/Api/KioskController.php
+++ app/Http/Controllers/Api/KioskController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\KioskModel;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+
 
 class KioskController extends Controller
 {
@@ -36,9 +39,12 @@ class KioskController extends Controller
         if($email !='' && $password != ''){
             $data = array("username"=>$email, "email"=>$email,"password"=>$password);
  
-            // Insert
+            // Insert user details into the KioskDB
             $value = KioskModel::insertData($data);
             if($value){
+                $KHH_BASE_PATH = env("KHH_STORAGE_BASE");
+                $KHH_user_folder_path = $KHH_BASE_PATH.'khh_user_'.$value;
+                File::makeDirectory($KHH_user_folder_path, $mode = 0777, true, true);
                 return response()->json(KioskModel::get()->where('user_id', $value),200);
             }else{
                 return response()->json(['user already exists']);
