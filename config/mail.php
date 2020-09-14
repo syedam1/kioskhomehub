5afa0b46d8d178ae44e9bfd29e5fc964dd5412af
syedam1 <syed@am1.org>
2020-09-14 12:16:26 +0100

    updated mailer with working configs for now

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,13 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'ses' => [
@@ -70,6 +77,13 @@ return [
         'array' => [
             'transport' => 'array',
         ],
+        'stream' => [
+            'ssl' => [
+               'allow_self_signed' => true,
+               'verify_peer' => false,
+               'verify_peer_name' => false,
+            ],
+         ],
     ],
 
     /*
@@ -84,8 +98,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'admin@am1.org'),
+        'name' => env('MAIL_FROM_NAME', 'KioskHomeHub'),
     ],
 
     /*
