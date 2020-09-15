1808c2b1588ff16e0e857abc193e7e98ff64436d
syedam1 <syed@am1.org>
2020-09-15 19:47:53 +0100

    updated home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,10 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/favs/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/images/favs/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/images/favs/favicon-16x16.png">
-    <link rel="manifest" href="/images/favs/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favs/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favs/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favs/favicon-16x16.png">
+    <link rel="manifest" href="/assets/img/favs/site.webmanifest">
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('assets/css/purpose.css') }} " id="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }} " id="stylesheet">
