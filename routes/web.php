c54915feb0e6a8b64d954300692bebe09c57e958
syedam1 <syed@am1.org>
2020-09-17 15:01:37 +0100

    testing

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,6 @@ Route::get('/account/payment-history', 'AccountController@paymenthistory')->name
 Route::get('/account/notifications', 'AccountController@notifications')->name('notifications');
 Route::get('/application/widgets', 'AccountController@widgets')->name('widgets');
 
-//Route::post('/account/profilesubmit', 'AccountController@profilesubmit')->name('profile-submit');
-Route::Post('/account/profilesubmit', function(){
-    
-    echo "<pre>", print_r($_REQUEST);
-    print_r($_FILES);
-
-})->name('profile-submit');
+Route::post('/account/profilesubmit', 'AccountController@profilesubmit')->name('profile-submit');
+//Route::Post('/account/profilesubmit', function(){echo "<pre>", print_r($_REQUEST);print_r($_FILES);})->name('profile-submit');
 
