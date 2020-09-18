5cf033a71b26758457204dcc753fbd75a45d5c26
syedam1 <syed@am1.org>
2020-09-18 17:08:23 +0100

    added aws credentials for testing

--- config/mail.php
+++ config/mail.php
@@ -43,13 +43,13 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
-            'stream' => [
+            /*'stream' => [
                 'ssl' => [
                     'allow_self_signed' => true,
                     'verify_peer' => false,
                     'verify_peer_name' => false,
                 ],
-            ],
+            ],*/
         ],
 
         'ses' => [
@@ -76,14 +76,7 @@ return [
 
         'array' => [
             'transport' => 'array',
-        ],
-        'stream' => [
-            'ssl' => [
-               'allow_self_signed' => true,
-               'verify_peer' => false,
-               'verify_peer_name' => false,
-            ],
-         ],
+        ]
     ],
 
     /*
