d4015d4c317e5d7dd71d3ae7c9098b5acd821744
syedam1 <syed@am1.org>
2020-09-28 12:39:56 +0100

    integration of Slack Button

--- resources/views/account/settings.blade.php
+++ resources/views/account/settings.blade.php
@@ -21,6 +21,24 @@
     <div class="row">
         @include('layouts.rightsidenav')
         <div class="col-lg-8 order-lg-1">
+        <!-- SLACK INTEGRATION -->
+        <div class="card">
+                <div class="card-header">
+                    <h5 class=" h6 mb-0">Connect Apps</h5>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook&client_id=1332390320279.1397740094980">
+                                <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" 
+                                srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- CHANGE PASSWORD -->
             <div class="card">
                 <div class="card-header">
                     <h5 class=" h6 mb-0">Change password</h5>
