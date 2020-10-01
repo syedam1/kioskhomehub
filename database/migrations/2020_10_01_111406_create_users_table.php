e7a147af2fb0c0597af01c63b3e104cc08df613f
syedam1 <syed@am1.org>
2020-10-01 12:19:12 +0100

    Migration Generated

--- database/migrations/2020_10_01_111406_create_users_table.php
+++ database/migrations/2020_10_01_111406_create_users_table.php
@@ -15,21 +15,21 @@ class CreateUsersTable extends Migration {
 	{
 		Schema::create('users', function(Blueprint $table)
 		{
-			$table->integer(''user_id'', true);
-			$table->string(''username'', 50);
-			$table->string(''email'', 50)->unique('unique_email');
+			$table->integer('user_id', true);
+			$table->string('username', 50);
+			$table->string('email', 50)->unique('unique_email');
 			$table->string('password');
 			$table->timestamp('created_date')->default(DB::raw('CURRENT_TIMESTAMP'));
 			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 			$table->integer('disabled')->default(0);
-			$table->string(''verification_code'', 50)->nullable();
+			$table->string('verification_code', 50)->nullable();
 			$table->integer('is_verified')->nullable()->default(0);
-			$table->string(''first_name'', 100)->nullable();
-			$table->string(''last_name'', 100)->nullable();
-			$table->string(''profile_image'', 100)->nullable();
-			$table->string(''bio'', 500)->nullable();
-			$table->string(''slack_verification_token'', 100)->nullable();
-			$table->string(''slack_access_token'', 100)->nullable();
+			$table->string('first_name', 100)->nullable();
+			$table->string('last_name', 100)->nullable();
+			$table->string('profile_image', 100)->nullable();
+			$table->string('bio', 500)->nullable();
+			$table->string('slack_verification_token', 100)->nullable();
+			$table->string('slack_access_token', 100)->nullable();
 		});
 	}
 
